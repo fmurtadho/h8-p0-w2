c4ad9a5bec3ee5ac784075c7973b6d880e4c8397
fmurtadho <fmurtadho@gmail.com>
2018-08-05 18:16:44 +0700

    perbaikan exercise 3,4,6,7

--- exercise-6.js
+++ exercise-6.js
@@ -1,77 +1,132 @@
 //1. Melakukan Looping Menggunakan While
-var nilaiAwal = 2
-console.log('1. Melakukan Looping Menggunakan While :');
-console.log('LOOPING PERTAMA');
-while(nilaiAwal <=20) {
-    console.log(nilaiAwal+ ' I love coding');
-nilaiAwal+= 2;
+/*
+OUTPUT :
+LOOPING PERTAMA
+2 - I love coding
+4 - I love coding
+6 - I love coding
+8 - I love coding
+10 - I love coding
+12 - I love coding
+14 - I love coding
+16 - I love coding
+18 - I love coding
+20 - I love coding
+LOOPING KEDUA
+20 - I will become fullstack developer
+18 - I will become fullstack developer                                                                              
+16 - I will become fullstack developer
+14 - I will become fullstack developer
+12 - I will become fullstack developer
+10 - I will become fullstack developer
+8 - I will become fullstack developer
+6 - I will become fullstack developer
+4 - I will become fullstack developer
+2 - I will become fullstack developer*/
+var input1=2
+var input2=20
+console.log('LOOPING PERTAMA')
+while(input1<=20){
+    console.log(input1+' - I love coding')
+    input1+=2
 }
 console.log('LOOPING KEDUA')
-var nilaiAwal2 = 20
-while(nilaiAwal2 >1) {
-    console.log(nilaiAwal2+' I will become fullstack developer');
-nilaiAwal2-= 2;
+while(input2>=2){
+    console.log(input2+' - I will become fullstack developer')
+    input2-=2
 }
-
-//spasi
-console.log();
-//spasi
 
 //2. Melakukan Looping Menggunakan For
-console.log('2. Melakukan Looping Menggunakan For :');
-console.log('LOOPING PERTAMA');
-for (var nilaiAwal = 1; nilaiAwal <= 20; nilaiAwal++ ) {
- console.log(nilaiAwal + ' - I love coding');
-}
-console.log('LOOPING KEDUA');
-for (var nilaiAwal2 = 20; nilaiAwal2 >0; nilaiAwal2-- ) {
-    console.log(nilaiAwal2 + ' - I will become fullstack developer');
+/*LOOPING PERTAMA
+1 - I love coding
+2 - I love coding
+3 - I love coding
+4 - I love coding
+5 - I love coding 
+6 - I love coding
+7 - I love coding
+8 - I love coding
+9 - I love coding
+10 - I love coding
+11 - I love coding
+12 - I love coding 
+13 - I love coding
+14 - I love coding
+15 - I love coding
+16 - I love coding
+17 - I love coding
+18 - I love coding
+19 - I love coding
+20 - I love coding
+LOOPING KEDUA
+20 - I will become fullstack developer
+19 - I will become fullstack developer
+18 - I will become fullstack developer
+17 - I will become fullstack developer
+16 - I will become fullstack developer
+15 - I will become fullstack developer
+14 - I will become fullstack developer
+13 - I will become fullstack developer
+12 - I will become fullstack developer
+11 - I will become fullstack developer
+10 - I will become fullstack developer
+9 - I will become fullstack developer
+8 - I will become fullstack developer
+7 - I will become fullstack developer
+6 - I will become fullstack developer
+5 - I will become fullstack developer
+4 - I will become fullstack developer
+3 - I will become fullstack developer
+2 - I will become fullstack developer
+1 - I will become fullstack developer
+*/
+
+console.log('LOOPING PERTAMA')
+for(i=1; i<=20; i++){
+    console.log(i+' - I love coding');
 }
 
-//spasi
-console.log();
-//spasi
+console.log('LOOPING KEDUA')
+for(j=20; j>=1; j--){
+    console.log(j+' - I will become fullstack developer')
+}
 
 //3. Angka Ganjil dan Genap
-console.log('3. Angka Ganjil dan Genap :');
-for(angkaAwal =1; angkaAwal <= 100; angkaAwal++) {
-if(angkaAwal % 2 == 0) {
-    console.log('Genap');
-} else if(angkaAwal % 1 == 0) {
-    console.log('Ganjil');
-}
-}
-//spasi
-console.log();
-//spasi
+/*Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+Di dalam perulangan, periksa setiap angka counter:
+Apabila angka counter adalah angka genap, tuliskan GENAP
+Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+*/
 
-//perulangan denga penambahan counter 2
-console.log('perulangan dengan penambahan counter 2 :');
-for (angkaAwal = 1; angkaAwal <=100; angkaAwal+= 2){
-    if(angkaAwal %3 == 0) {
-        console.log(angkaAwal+' KELIPATAN 3');
+for(i=1; i<=100; i++){
+    if(i % 2 == 0){
+        console.log('GENAP')
+    }else{
+        console.log('GANJIL')
     }
 }
 
-//spasi
-console.log();
-//spasi
+/*Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
+Pada 3 perulangan baru ini periksa setiap angka counter:
+Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
+Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
+"3 kelipatan 3" dan seterusnya.
+*/
 
-//perulangan dengan penambahan counter 5
-console.log('perulangan dengan penambahan counter 5 :');
-for(angkaAwal = 1; angkaAwal <=100; angkaAwal+= 5) {
-    if(angkaAwal %6 == 0) {
-        console.log(angkaAwal+' KELIPATAN 6');
+for(i=1;i<=100;i+=2){
+    if(i % 3 == 0){
+        console.log(i+' KELIPATAN 3')
     }
 }
-//spasi
-console.log();
-//spasi
 
-//perulangan dengan penambahan counter 9
-console.log('perulangan dengan penambahan counter 9 :');
-for(angkaAwal = 1; angkaAwal <=100; angkaAwal+=9) {
-    if(angkaAwal %10 ==0) {
-        console.log(angkaAwal+'KELIPATAN 10')
+for(j=1;j<=100;j+=5){
+    if(j % 6 == 0){
+        console.log(j+' KELIPATAN 6')
+    }
+}
+
+for(k=1;k<=100;k+=9){
+    if(k % 10 == 0){
+        console.log(k+' KELIPATAN 10')
     }
 }
--- exercise-7.js
+++ exercise-7.js
@@ -1,67 +1,63 @@
-// 1. Menyusun Barisan Bintang
-/*
-  Output:
-  
-  Jika rows1 = 5
-  *
-  *
-  *
-  *
-  *
+//1. Menyusun Barisan Bintang
+/*Output
+jika rows1 = 5
+
+*
+*
+*
+*
+*
 */
-console.log('1. Menyusun Barisan Bintang');
-var rows1 = 5;
-for(asterisks = 1; asterisks <= rows1 ; asterisks++)
-{
-    console.log('*');
+var rows1 = 5
+
+for(i=1;i<=rows1;i++){
+
+    console.log('*')
 }
 
-console.log();//spasi
+//2. Menyusun Barisan Bintang Dengan Nested Looping
+/*Output
+jika rows2 = 5
 
-// 2. Menyusun Barisan Bintang Dengan Nested Looping
-/*
-  Output:
-  Jika rows2 = 5
-  *****
-  *****
-  *****
-  *****
-  *****
+*****
+*****
+*****
+*****
+*****
 */
-console.log('2. Menyusun Barisan Bintang Dengan Nested Looping')
-var rows2 = 7;
-for (asterisks = 1; asterisks <= rows2; asterisks++) 
-{
-    var hasilAst = ""; 
-    for (asterisks2 = 1; asterisks2 <= rows2; asterisks2++) 
-    {
-        hasilAst += "*"; 
+
+var rows2 = 5
+
+for(i=1;i<=rows2;i++) {
+
+    let output =''
+
+    for(j=1;j<=rows2;j++) {
+        
+        output += '*'
+
     }
-  console.log(hasilAst);
+    console.log(output)
 }
 
-console.log();//spasi
+//3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+/*jika rows3 = 5
 
-// 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
-/*
-  Output:
-  Jika rows3 = 5
-  *
-  **
-  ***
-  ****
-  *****
+*
+**
+***
+****
+*****
 */
-console.log('3. Menyusun Barisan Tangga Bintang Dengan Nested Looping');
 var rows3 = 5
-for(asterisks = 1; asterisks <= rows3; asterisks++) {
-    var hasilAst = ''
-    {
-        var hasilAst = ""; 
-        for (asterisks2 = 1; asterisks2 <= asterisks; asterisks2++) 
-        {
-            hasilAst += "*"; 
-        }
-      console.log(hasilAst);
+
+for(i=1;i<=rows3;i++){
+
+    let output=''
+
+    for(j=1;j<=i;j++){
+
+        output += '*'
     }
+    console.log(output)
 }
